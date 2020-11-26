9ecc552caa7e9c37c035f30a72621c0568e9bcbe
swnishan <swnishan@gmail.com>
2020-11-26 21:53:37 +0100

    create activity to show

---
+++ sample/src/main/java/com/swnishan/materialdatetimepicker/sample/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.swnishan.materialdatetimepicker.sample
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        show.setOnClickListener {
+
+        }
+    }
+}
