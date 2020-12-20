68735eb9f1e25625b3b9925fffe350259cfe8046
swnishan <swnishan@gmail.com>
2020-12-20 23:19:08 +0100

    add TimePickerAdapter and item

---
+++ materialdatetimepicker/src/main/java/com/swnishan/materialdatetimepicker/timepicker/TimePickerAdapter.kt
@@ -0,0 +1,36 @@
+package com.swnishan.materialdatetimepicker.timepicker
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.swnishan.materialdatetimepicker.R
+
+internal class TimePickerAdapter(private val items: List<Int>) : RecyclerView.Adapter<TimePickerViewHolder>() {
+
+    private var selectedTime: Int = 0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TimePickerViewHolder {
+        return TimePickerViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_time_picker, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: TimePickerViewHolder, position: Int) {
+        // Here get actual element from the items by position%items.size
+        holder.bind(items[position % items.size], selectedTime == position)
+    }
+
+    /**
+     * To make recycler view repeatable here set the item count as Int.MAX_VALUE.
+     * So, it seems there's lots of items but the actual item count is items.size
+     */
+    override fun getItemCount(): Int = Int.MAX_VALUE
+
+    fun setSelectedTime(time: Int) {
+        this.selectedTime = time
+        notifyDataSetChanged()
+    }
+
+    fun getSelectedTime() = selectedTime
+}
---
+++ materialdatetimepicker/src/main/java/com/swnishan/materialdatetimepicker/timepicker/TimePickerViewHolder.kt
@@ -0,0 +1,17 @@
+package com.swnishan.materialdatetimepicker.timepicker
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.swnishan.materialdatetimepicker.R
+
+
+internal class TimePickerViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+    fun bind(time: Int, isSelected: Boolean) {
+        (view as TextView).text = String.format("%02d", time)
+
+        val color = if (isSelected) R.color.black else R.color.O700
+        view.setTextColor(view.context.resources.getColor(color))
+    }
+}
