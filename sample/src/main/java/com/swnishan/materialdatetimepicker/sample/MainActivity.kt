67cda6547745a8f0e28efd56625c95d5782e4cfe
swnishan <swnishan@gmail.com>
2020-12-20 23:20:01 +0100

    show TimePickerDialog

--- sample/src/main/java/com/swnishan/materialdatetimepicker/sample/MainActivity.kt
+++ sample/src/main/java/com/swnishan/materialdatetimepicker/sample/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.swnishan.materialdatetimepicker.sample
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import com.swnishan.materialdatetimepicker.timepicker.TimePickerDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
         show.setOnClickListener {
-
+            TimePickerDialog.create("set", "Ok", "Cancel", null).show(supportFragmentManager, "")
         }
     }
 }
