1c3c2b1ab0db164d83673d7dbff8c874db77567d
swnishan <swnishan@gmail.com>
2020-12-30 14:52:08 +0100

    save state of the MaterialTimePickerDialog.kt

--- materialdatetimepicker/src/main/java/com/swnishan/materialdatetimepicker/timepicker/MaterialTimePickerDialog.kt
+++ materialdatetimepicker/src/main/java/com/swnishan/materialdatetimepicker/timepicker/MaterialTimePickerDialog.kt
@@ -6,17 +6,21 @@ import android.os.Bundle
 import android.util.TypedValue
 import android.view.ContextThemeWrapper
 import androidx.annotation.AttrRes
+import androidx.annotation.IntRange
+import androidx.annotation.StyleRes
 import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.swnishan.materialdatetimepicker.R
 import com.swnishan.materialdatetimepicker.timepicker.view.MaterialTimePickerView
+import org.threeten.bp.LocalTime
 
 class MaterialTimePickerDialog : DialogFragment() {
 
     private var onTimePickedListener: MaterialTimePickerView.OnTimePickedListener? = null
     private var materialTimePickerView: MaterialTimePickerView? = null
     private var clockType = MaterialTimePickerView.TimeConvention.HOURS_24
+    private var pickerTime: LocalTime = LocalTime.now()
     private var themeRes= R.style.ThemeOverlay_Dialog_MaterialTimePicker
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -25,8 +29,8 @@ class MaterialTimePickerDialog : DialogFragment() {
         val timePickerThemeContext = ContextThemeWrapper(builder.context, timePickerViewStyle)
         materialTimePickerView = MaterialTimePickerView(context=timePickerThemeContext)
         materialTimePickerView?.setTimeConvention(clockType)
+        materialTimePickerView?.setTime(pickerTime.hour, pickerTime.minute)
         materialTimePickerView?.setOnTimePickedListener(onTimePickedListener)
-        materialTimePickerView?.setTime(10,10)
 
         builder.apply {
             setView(materialTimePickerView)
@@ -43,11 +47,32 @@ class MaterialTimePickerDialog : DialogFragment() {
         typedValue.resourceId
     }
 
-    fun setClockType(timeConvention: MaterialTimePickerView.TimeConvention){
-        this.clockType = timeConvention
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        restoreState(savedInstanceState?:arguments)
     }
 
-    override fun setStyle(style: Int, theme: Int) {
+    private fun restoreState(bundle: Bundle?) {
+        if (bundle==null) return
+        themeRes=bundle.getInt(ARG_THEME, themeRes)
+
+        val hour =bundle.getInt(ARG_HOUR, pickerTime.hour)
+        val minute=bundle.getInt(ARG_MINUTE, pickerTime.minute)
+        pickerTime=pickerTime.withHour(hour).withMinute(minute)
+
+        clockType=MaterialTimePickerView.TimeConvention.valueOf(bundle.getString(ARG_TIME_CONVENTION, clockType.name))
+    }
+
+    override fun onSaveInstanceState(bundle: Bundle) {
+        super.onSaveInstanceState(bundle)
+
+        bundle.putInt(ARG_THEME, themeRes)
+        bundle.putInt(ARG_HOUR, materialTimePickerView?.getHour()?:pickerTime.hour)
+        bundle.putInt(ARG_MINUTE, materialTimePickerView?.getMinute()?:pickerTime.minute)
+        bundle.putString(ARG_TIME_CONVENTION, clockType.name)
+    }
+
+     override fun setStyle(style: Int, theme: Int) {
         themeRes=theme
         super.setStyle(style, theme)
     }
@@ -59,7 +84,6 @@ class MaterialTimePickerDialog : DialogFragment() {
     fun setOnTimePickListener(listener:MaterialTimePickerView.OnTimePickedListener?) {
          onTimePickedListener=listener
     }
-
 
     object Builder{
         private val timePickerDialog=MaterialTimePickerDialog()
@@ -81,7 +105,18 @@ class MaterialTimePickerDialog : DialogFragment() {
         }
 
         fun setClockType(timeConvention: MaterialTimePickerView.TimeConvention): Builder {
-            timePickerDialog.setClockType(timeConvention)
+            bundle.putString(ARG_TIME_CONVENTION, timeConvention.name)
+            return this
+        }
+
+        fun setTime(@IntRange(from =0 ,to=23)hour: Int, @IntRange(from = 0, to = 60) minute: Int): Builder {
+            bundle.putInt(ARG_HOUR, hour)
+            bundle.putInt(ARG_MINUTE, minute)
+            return this
+        }
+
+        fun setTheme(@StyleRes themeRes:Int): Builder {
+            bundle.putInt(ARG_THEME, themeRes)
             return this
         }
 
@@ -95,6 +130,10 @@ class MaterialTimePickerDialog : DialogFragment() {
         private const val ARG_POSITIVE_BUTTON_TEXT = "arg_positive_button_text"
         private const val ARG_NEGATIVE_BUTTON_TEXT = "arg_negative_button_text"
         private const val ARG_TITLE = "arg_title"
+        private const val ARG_THEME = "arg_theme"
+        private const val ARG_TIME_CONVENTION = "arg_time_convention"
+        private const val ARG_HOUR = "arg_hour"
+        private const val ARG_MINUTE = "arg_minute"
     }
 
 }
