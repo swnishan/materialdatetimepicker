fbf66f559c3d9c3bfc6ac77c3c3f63fb53853137
swnishan <swnishan@gmail.com>
2021-02-06 18:46:34 +0100

    spotless

--- sample/src/main/java/com/swnishan/materialdatetimepicker/sample/MainActivity.kt
+++ sample/src/main/java/com/swnishan/materialdatetimepicker/sample/MainActivity.kt
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
